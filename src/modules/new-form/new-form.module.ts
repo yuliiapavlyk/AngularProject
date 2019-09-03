5da786c14eaf6679ed83213093b82623642e345f
Yuliia Pavlyk <ypavly@softserveinc.com>
2019-09-03 18:16:18 +0300

    initial commit for new-form page

--- src/modules/new-form/new-form.module.ts
+++ src/modules/new-form/new-form.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
-
-import { UserProfileComponent } from '../pages/user-profile/user-profile.component';
+import { NewFormComponent } from 'src/pages/new-form/new-form.component';
 
 const routes: Route[] = [
   {
     path: '',
-    component: UserProfileComponent,
+    component: NewFormComponent,
   },
 ];
 
 @NgModule({
-  declarations: [UserProfileComponent],
+  declarations: [NewFormComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class UserProfileModule {}
+export class NewFormModule { }
