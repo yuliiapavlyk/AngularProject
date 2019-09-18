c4043c9480ae9a90dbc86071c83c425395d9c626
yuliiapavlyk <pavlyk.yuliia@gmail.com>
2019-09-18 23:29:23 +0300

    initial commit

--- src/pages/user-profile/user-profile.component.ts
+++ src/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+    private userService: UserService,) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.compose([Validators.email, Validators.required]) ],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirm: ['']});
   }
 
 }
