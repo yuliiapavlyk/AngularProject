51588d80aca0ade2e44369d428b676928c0dbd08
yuliiapavlyk <pavlyk.yuliia@gmail.com>
2019-09-29 23:21:05 +0300

    added user profile updating

--- src/modules/user-profile.module.ts
+++ src/modules/user-profile.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserProfileComponent } from '../pages/user-profile/user-profile.component';
 
 const routes: Route[] = [
@@ -13,6 +14,6 @@ const routes: Route[] = [
 
 @NgModule({
   declarations: [UserProfileComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes),FormsModule,ReactiveFormsModule],
 })
 export class UserProfileModule {}
